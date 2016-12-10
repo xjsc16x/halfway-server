13eb0e072b67eeb2ed98c1c98df6005a6bd2c81c
Justin Charron <xjsc16x@gmail.com>
2016-12-09 21:32:50 -0500

    updated to lastest development - server works / testing broken

--- test/test.js
+++ test/test.js
@@ -1,57 +1,51 @@
 var should = require('should'); 
 var assert = require('assert');
-var request = require('supertest');  
-var mongoose = require('mongoose');
-var winston = require('winston');
+var request = require('supertest'); 
 
 describe('Firebase database', function() {
-	it('should be able to get the test user', function(done) {
-      var profile = {
-        username: 'vgheri',
-        password: 'test',
-        firstName: 'Valerio',
-        lastName: 'Gheri'
-      };
-    // once we have specified the info we want to send to the server via POST verb,
-    // we need to actually perform the action on the resource, in this case we want to 
-    // POST on /api/profiles and we want to send some info
-    // We do this using the request object, requiring supertest!
-    request(url)
-	.post('/api/profiles')
-	.send(profile)
-    // end handles the response
-	.end(function(err, res) {
-          if (err) {
-            throw err;
-          }
-          // this is should.js syntax, very clear
-          res.should.have.status(400);
-          done();
+    it('should be able to make a test user', function(done) {
+        var url = "localhost:3000";
+        var userData = {
+            username: 'test',
+            name: 'Test User',
+            location: { 'latitude': 30, 'longitude': 70 }
+        };
+
+        // send request
+        request(url)
+            .post('/testapi/user')
+            .send(userData)
+            // end handles the response
+            .end(function(err, res) {
+                if (err) {
+                    throw err;
+                }
+                // this is should.js syntax, very clear
+                res.should.have.status(200);
+                done();
         });
     });
 
-	it('should be able to get the test group', function(done) {
-      var profile = {
-        username: 'vgheri',
-        password: 'test',
-        firstName: 'Valerio',
-        lastName: 'Gheri'
-      };
-    // once we have specified the info we want to send to the server via POST verb,
-    // we need to actually perform the action on the resource, in this case we want to 
-    // POST on /api/profiles and we want to send some info
-    // We do this using the request object, requiring supertest!
-    request(url)
-	.post('/api/profiles')
-	.send(profile)
-    // end handles the response
-	.end(function(err, res) {
-          if (err) {
-            throw err;
-          }
-          // this is should.js syntax, very clear
-          res.should.have.status(400);
-          done();
+    it('should be able to get a test user', function(done) {
+        var url = "localhost:3000";
+        var userData = {
+            username: 'test',
+            name: 'Test User',
+            location: { 'latitude': 30, 'longitude': 70 }
+        };
+
+        // send request
+        request(url)
+            .post('/testapi/user')
+            .send(userData)
+            // end handles the response
+            .end(function(err, res) {
+                if (err) {
+                    throw err;
+                }
+                // this is should.js syntax, very clear
+                res.should.have.status(200);
+                done();
         });
     });
 });
